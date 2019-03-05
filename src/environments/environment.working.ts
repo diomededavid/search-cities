56ecc129cc58c7cd3051c84dcf5459e6eb3bccba
David Diomede <david@d2creativedesign.com>
2019-03-05 16:44:57 -0700

    new commits

--- src/environments/environment.working.ts
+++ src/environments/environment.working.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   service: {
-    apiKey: '',
+    apiKey: 'f43d6b356amshdf56e87f9a9e904p1e3315jsn480c38d11bf2',
     uri: 'http://geodb-free-service.wirefreethought.com'
   }
 };
